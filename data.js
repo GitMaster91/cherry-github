942de9c6b18647262c23e6665f5173c23c0f33e5
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-28 11:07:54 -0600

    all data

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
 const arrDev1 = [
-   { id: 1, data: ['Luis Alberto'] }
+   { id: 1, data: ['Luis Alberto'] },
+   { id: 2, data: ['Leydi Lima']   },
+   { id: 3, data: ['USER 3']       },
+   { id: 4, data: ['USER 4']       },
 ];
